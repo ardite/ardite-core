b79104af7519b0b01a93024400ab0e1a5bd6e38d
calebmer <calebmeredith8@gmail.com>
2016-02-06 18:24:05 -0500

    feat: add get database function

--- src/database.rs
+++ src/database.rs
@@ -5,6 +5,16 @@ use error::Error;
 use value::*;
 use query::Query;
 
+/// Gets the database from a URL string using the protocol. For example a URL
+/// of `postgres://localhost:5432/test_db` would look for a
+/// `ardite-database-postgres` crate, download the crate if it did not already
+/// exist in the file system, and then return an instance initialized with the
+/// `connect` static trait function.
+pub fn get_database<D: Database>(url: &str) -> D {
+  // TODO: implement
+  unimplemented!();
+}
+
 pub trait Database {
   /// Connects to a database and returns a driver instance. After calling this
   /// the driver is ready to roll!
