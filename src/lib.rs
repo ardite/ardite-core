6f7d7fcdc7ea6427ba7c3077e511403a04d81fb5
calebmer <calebmeredith8@gmail.com>
2016-02-27 21:28:17 -0500

    feat: add query validation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-extern crate linear_map;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate linear_map;
+extern crate regex;
 
 pub mod driver;
 pub mod error;
-pub mod value;
 pub mod query;
+pub mod value;
--- src/query.rs
+++ src/query.rs
@@ -17,7 +17,7 @@ pub enum Query {
 
 impl From<Pointer> for Query {
   fn from(pointer: Pointer) -> Self {
-    // Reverse loop through the pointer to construct the query. 
+    // Reverse loop through the pointer to construct the query.
     pointer.iter().rev().fold(Query::Value, |acc, key| {
       let mut map = LinearMap::new();
       map.insert(key.to_owned(), acc);
@@ -28,35 +28,23 @@ impl From<Pointer> for Query {
 
 #[cfg(test)]
 mod tests {
-  use linear_map::LinearMap;
   use query::Query;
-  
-  fn new_map<K: Eq, V>(vec: Vec<(K, V)>) -> LinearMap<K, V> {
-    let mut map = LinearMap::new();
-    for (k, v) in vec {
-      map.insert(k, v);
-    }
-    map
-  }
-  
+
   #[test]
-  fn from_pointer() {
+  fn test_from_pointer() {
     let hello = || String::from("hello");
     let good = || String::from("good");
-    let world = || String::from("world");    
-    
-    assert_eq!(Query::from(vec![hello(), good(), world()]), Query::Object(new_map(vec![
-      (hello(), Query::Object(new_map(vec![
-        (good(), Query::Object(new_map(vec![
-          (world(), Query::Value)
-        ])))
-      ])))
-    ])));
-    
-    assert_eq!(Query::from(vec![good()]), Query::Object(new_map(vec![
-      (good(), Query::Value)
-    ])));
-    
+    let world = || String::from("world");
+    assert_eq!(Query::from(vec![hello(), good(), world()]), Query::Object(linear_map!{
+      String::from("hello") => Query::Object(linear_map!{
+        String::from("good") => Query::Object(linear_map!{
+          String::from("world") => Query::Value
+        })
+      })
+    }));
+    assert_eq!(Query::from(vec![good()]), Query::Object(linear_map!{
+      String::from("good") => Query::Value
+    }));
     assert_eq!(Query::from(vec![]), Query::Value);
   }
 }
