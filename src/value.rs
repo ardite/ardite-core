aad7b74ca23ef0b05e1e5bb5a23870a048cdcf8e
calebmer <calebmeredith8@gmail.com>
2016-02-28 16:28:21 -0500

    feat: add value get function

--- src/value.rs
+++ src/value.rs
@@ -15,7 +15,7 @@ pub type Pointer = Vec<Key>;
 /// 5).
 ///
 /// [1]: http://ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum Value {
   /// The abscense of any value.
   Null,
@@ -30,4 +30,80 @@ pub enum Value {
   Object(LinearMap<Key, Value>),
   /// A list of values. Just a value, but using *only* integer keys.
   Array(Vec<Value>)
+}
+
+impl Value {
+  /// Gets a value at a specific point. Helpful for retrieving nested values.
+  pub fn get(&self, mut pointer: Pointer) -> Option<Value> {
+    match self {
+      &Value::Null => if pointer.len() == 0 { Some(Value::Null) } else { None },
+      &Value::Boolean(_) => if pointer.len() == 0 { Some(self.clone()) } else { None },
+      &Value::Number(_) => if pointer.len() == 0 { Some(self.clone()) } else { None },
+      &Value::String(_) => if pointer.len() == 0 { Some(self.clone()) } else { None },
+      &Value::Object(ref map) => {
+        if pointer.len() == 0 {
+          Some(self.clone())
+        } else {
+          let key = pointer.remove(0);
+          if let Some(value) = map.get(&key) {
+            value.get(pointer)
+          } else {
+            None
+          }
+        }
+      },
+      &Value::Array(ref vec) => {
+        if pointer.len() == 0 {
+          Some(self.clone())
+        } else {
+          let key = pointer.remove(0);
+          if let Some(index) = key.parse::<usize>().ok() {
+            if let Some(value) = vec.get(index) {
+              value.get(pointer)
+            } else {
+              None
+            }
+          } else {
+            None
+          }
+        }
+      }
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn test_get_primitive() {
+    assert_eq!(vnull!().get(point![]), Some(vnull!()));
+    assert_eq!(vnull!().get(point!["hello"]), None);
+    assert_eq!(vbool!(true).get(point![]), Some(vbool!(true)));
+    assert_eq!(vbool!(true).get(point!["hello"]), None);
+    assert_eq!(vnum!(36f64).get(point![]), Some(vnum!(36f64)));
+    assert_eq!(vnum!(36f64).get(point!["hello"]), None);
+    assert_eq!(vstring!("world").get(point![]), Some(vstring!("world")));
+    assert_eq!(vstring!("world").get(point!["hello"]), None);
+  }
+
+  #[test]
+  fn test_get_object() {
+    let object = vobject!{
+      "hello" => vbool!(true),
+      "world" => vnum!(8f64),
+      "5" => vnull!(),
+      "moon" => vobject!{
+        "hello" => vstring!("yoyo")
+      }
+    };
+    assert_eq!(object.get(point![]), Some(object.clone()));
+    assert_eq!(object.get(point!["hello"]), Some(vbool!(true)));
+    assert_eq!(object.get(point!["5"]), Some(vnull!()));
+    assert_eq!(object.get(point!["world", "hello"]), None);
+    assert_eq!(object.get(point!["moon", "hello"]), Some(vstring!("yoyo")));
+    assert_eq!(object.get(point!["moon", "nope"]), None);
+  }
+
+  // #[test]
+  // fn test_get_array
 }
