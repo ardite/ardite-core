80737ef35e7806c7074586ec7b4b5c952d6098b5
calebmer <calebmeredith8@gmail.com>
2016-03-08 16:58:28 -0500

    refactor: don't make pub what doesn't have to be

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -2,9 +2,9 @@
 //! Definition, a format which is used to imperitevly define the data interface
 //! used with Ardite services. 
 
-pub mod definition;
-pub mod schema;
-pub mod serde;
+mod definition;
+mod schema;
+mod serde;
 
 pub use schema::definition::Definition;
 pub use schema::schema::Schema;
