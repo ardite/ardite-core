fe8307fca2230c132af398c5812024c27071a21b
calebmer <calebmeredith8@gmail.com>
2016-03-11 16:51:16 -0500

    refactor: remove distracting trait methods

--- src/driver/driver.rs
+++ src/driver/driver.rs
@@ -1,47 +1,9 @@
 //! The driver trait which all drivers will implement.
 
-use schema::Definition;
-use error::{Error, ErrorCode};
-use patch::Patch;
-use value::{Pointer, Value};
-use query::Query;
+use error::Error;
 
 pub trait Driver {
   /// Connects to a driver and returns a driver instance. After calling this
   /// the driver is ready to roll!
   fn connect(uri: &str) -> Result<Self, Error> where Self: Sized;
-
-  /// Validates an Ardite Schema Definition dependending on the driver’s
-  /// contracts with the developer. Note that the definition object will not be
-  /// associated with the driver object. This is intentional, the driver should
-  /// not be able to influence the definition without an external tool.
-  fn validate_definition(_: &Definition) -> Result<(), Error> {
-    Ok(())
-  }
-
-  /// Performs a complex query on the driver. Returns a value whose shape
-  /// matches the shape of the query.
-  fn query(&self, query: Query) -> Result<Value, Error>;
-
-  /// Applies multiple patches to the driver. If one patch fails, all other
-  /// patches must also fail. Returns a value with all of the new patched
-  /// values along with any driver generated values only.
-  fn patch(&self, patch: Vec<Patch>) -> Result<Value, Error>;
-  
-  /// Get’s a value in the driver at a specific point and returns exactly that
-  /// value.
-  fn get(&self, pointer: Pointer) -> Result<Value, Error> {
-    let query = Query::from(pointer.clone());
-    let value = try!(self.query(query));
-    match value.get(pointer) {
-      Some(value) => Ok(value),
-      None => Err(Error::new(ErrorCode::Internal, "Driver failed to return a value with the requested data.", None))
-    }
-  }
-  
-  /// Set a value at a certain point in the driver. Returns nothing.
-  fn set(&self, pointer: Pointer, value: Value) -> Result<(), Error> {
-    try!(self.patch(vec![Patch::Set(pointer, value)]));
-    Ok(())
-  }
 }
