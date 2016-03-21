7faff1acef7ba0298076df188a170c626b74a902
calebmer <calebmeredith8@gmail.com>
2016-03-20 21:52:13 -0400

    feat(value): add to/from JSON methods

--- src/value.rs
+++ src/value.rs
@@ -8,6 +8,9 @@ use linear_map::serde::LinearMapVisitor;
 use serde::ser::{Serialize, Serializer};
 use serde::de::{Deserialize, Deserializer, Error as DeError, Visitor, SeqVisitor, MapVisitor};
 use serde::de::impls::VecVisitor;
+use serde_json;
+
+use error::Error;
 
 /// The type which represents the key for maps used throughout the Ardite
 /// codebase.
@@ -81,40 +84,19 @@ impl Value {
     }
   }
 
-  /// Converts a value into a JSON string for distribution.
-  pub fn to_json(&self) -> String {
-    match *self {
-      Value::Null => "null".to_owned(),
-      Value::Boolean(value) => if value { "true".to_owned() } else { "false".to_owned() },
-      Value::I64(value) => value.to_string(),
-      Value::F64(value) => value.to_string(),
-      Value::String(ref value) => "\"".to_owned() + &escape_string_for_json(value) + "\"",
-      Value::Object(ref map) => {
-        let mut json = "{".to_owned();
-        for (key, value) in map {
-          json.push_str("\"");
-          json.push_str(&escape_string_for_json(key));
-          json.push_str("\":");
-          json.push_str(&value.to_json());
-          json.push_str(",");
-        }
-        // Remove the last comma.
-        json.pop();
-        json.push_str("}");
-        json
-      },
-      Value::Array(ref vec) => {
-        let mut json = "[".to_owned();
-        for item in vec {
-          json.push_str(&item.to_json());
-          json.push_str(",");
-        }
-        // Remove the last comma.
-        json.pop();
-        json.push_str("]");
-        json
-      }
-    }
+  /// Creates a `Value` from a JSON string.
+  pub fn from_json(json: &str) -> Result<Value, Error> {
+    serde_json::from_str(json).map_err(Error::from)
+  }
+
+  /// Converts a `Value` into a JSON string.
+  pub fn to_json(&self) -> Result<String, Error> {
+    serde_json::to_string(self).map_err(Error::from)
+  }
+
+  /// Converts a `Value` into a nice and indented JSON string.
+  pub fn to_json_pretty(&self) -> Result<String, Error> {
+    serde_json::to_string_pretty(self).map_err(Error::from)
   }
 }
 
@@ -142,6 +124,7 @@ impl Deserialize for Value {
       type Value = Value;
 
       #[inline] fn visit_bool<E>(&mut self, value: bool) -> Result<Value, E> { Ok(Value::Boolean(value)) }
+      #[inline] fn visit_u64<E>(&mut self, value: u64) -> Result<Value, E> { Ok(Value::I64(value as i64)) }
       #[inline] fn visit_i64<E>(&mut self, value: i64) -> Result<Value, E> { Ok(Value::I64(value)) }
       #[inline] fn visit_f64<E>(&mut self, value: f64) -> Result<Value, E> { Ok(Value::F64(value)) }
       #[inline] fn visit_str<E>(&mut self, value: &str) -> Result<Value, E> where E: DeError { self.visit_string(value.to_owned()) }
@@ -196,12 +179,6 @@ impl<'a> From<&'a str> for Value {
   }
 }
 
-/// Takes a string and escapes it for use within a JSON encoded object. Read,
-/// inside quotes.
-fn escape_string_for_json(string: &str) -> String {
-  string.replace("\"", "\\\"").replace("\n", "\\n")
-}
-
 /// An iterator of values. Used by drivers to convert their own iterator
 /// implementations into a single type.
 pub struct ValueIter<'a> {
@@ -228,6 +205,8 @@ impl<'a> Iterator for ValueIter<'a> {
 
 #[cfg(test)]
 mod tests {
+  use value::Value;
+
   #[test]
   fn test_get_primitive() {
     assert_eq!(value!().get(point![]).cloned(), Some(value!()));
@@ -285,13 +264,35 @@ mod tests {
   }
 
   #[test]
+  fn test_from_json() {
+    assert_eq!(Value::from_json("null").unwrap(), value!());
+    assert_eq!(Value::from_json("true").unwrap(), value!(true));
+    assert_eq!(Value::from_json("false").unwrap(), value!(false));
+    assert_eq!(Value::from_json("7").unwrap(), value!(7));
+    assert_eq!(Value::from_json("3.3").unwrap(), value!(3.3));
+    assert_eq!(Value::from_json(r#""Hello,\n\"world\"!""#).unwrap(), value!("Hello,\n\"world\"!"));
+    assert_eq!(Value::from_json(r#"{"hello":"world","foo":true,"null":null,"goodbye":{"moon":2}}"#).unwrap(), value!({
+      "hello" => "world",
+      "foo" => true,
+      "null" => (),
+      "goodbye" => {
+        "moon" => 2
+      }
+    }));
+    assert_eq!(
+      Value::from_json(r#"["world",3.3,{"hello":"world"},null,null,[1,2,3],null]"#).unwrap(),
+      value!(["world", 3.3, { "hello" => "world" }, (), (), [1, 2, 3], ()])
+    );
+  }
+
+  #[test]
   fn test_to_json() {
-    assert_eq!(&value!().to_json(), "null");
-    assert_eq!(&value!(true).to_json(), "true");
-    assert_eq!(&value!(false).to_json(), "false");
-    assert_eq!(&value!(7).to_json(), "7");
-    assert_eq!(&value!(6.667).to_json(), "6.667");
-    assert_eq!(&value!("Hello,\n\"world\"!").to_json(), r#""Hello,\n\"world\"!""#);
+    assert_eq!(&value!().to_json().unwrap(), "null");
+    assert_eq!(&value!(true).to_json().unwrap(), "true");
+    assert_eq!(&value!(false).to_json().unwrap(), "false");
+    assert_eq!(&value!(7).to_json().unwrap(), "7");
+    assert_eq!(&value!(6.667).to_json().unwrap(), "6.667");
+    assert_eq!(&value!("Hello,\n\"world\"!").to_json().unwrap(), r#""Hello,\n\"world\"!""#);
     assert_eq!(&value!({
       "hello" => "world",
       "foo" => true,
@@ -299,10 +300,18 @@ mod tests {
       "goodbye" => {
         "moon" => 2
       }
-    }).to_json(), r#"{"hello":"world","foo":true,"null":null,"goodbye":{"moon":2}}"#);
+    }).to_json().unwrap(), r#"{"hello":"world","foo":true,"null":null,"goodbye":{"moon":2}}"#);
     assert_eq!(
-      &value!(["world", 3.333, { "hello" => "world" }, (), (), [1, 2, 3], ()]).to_json(),
+      &value!(["world", 3.333, { "hello" => "world" }, (), (), [1, 2, 3], ()]).to_json().unwrap(),
       r#"["world",3.333,{"hello":"world"},null,null,[1,2,3],null]"#
+    );
+  }
+
+  #[test]
+  fn test_to_json_pretty() {
+    assert_eq!(
+      &value!(["world", 3.333, { "hello" => "world" }, (), (), [1, 2, 3], ()]).to_json_pretty().unwrap(),
+      "[\n  \"world\",\n  3.333,\n  {\n    \"hello\": \"world\"\n  },\n  null,\n  null,\n  [\n    1,\n    2,\n    3\n  ],\n  null\n]"
     );
   }
 }
