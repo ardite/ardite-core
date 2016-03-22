ae06ec3902f0bd6b13e323056342929c2baf61ca
calebmer <calebmeredith8@gmail.com>
2016-03-22 08:10:40 -0400

    feat(driver): add new method

--- src/driver/memory.rs
+++ src/driver/memory.rs
@@ -1,3 +1,5 @@
+use url::Url;
+
 use driver::Driver;
 use error::Error;
 use query::{Range, SortRule, Condition, Query};
@@ -5,9 +7,15 @@ use value::{Key, ValueIter};
 
 struct MemoryDriver;
 
+impl MemoryDriver {
+  pub fn new() -> Self {
+    MemoryDriver
+  }
+}
+
 impl Driver for MemoryDriver {
-  fn connect(_: &str) -> Result<Self, Error> {
-    Ok(MemoryDriver)
+  fn connect(_: &Url) -> Result<Self, Error> {
+    Err(Error::invalid("You can’t connect to memory silly.", "Use the `new` method instead for the memory driver."))
   }
 
   fn read(
